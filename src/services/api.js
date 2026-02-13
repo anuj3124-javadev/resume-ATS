0e836c89e7770190310a8b1edcbcf2c7ccdef557
anuj3124-javadev <anujyadavst24@gmail.com>
2026-02-13 12:22:41 +0530

    update

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Create axios instance
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || '',
+  baseURL: process.env.REACT_APP_API_URL || 'https://resume-atsb.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
